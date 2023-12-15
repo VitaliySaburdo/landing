02eca7cb60869e9f729623d7ce987ae1bf72c74e
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-15 14:19:39 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const notify = require("gulp-notify");
 const webpack = require("gulp-webpack");
 const babel = require("gulp-babel");
 const imagemin = require("gulp-imagemin");
+const changed = require("gulp-changed");
 
 const fileIncludeSetting = {
   prefix: "@@",
@@ -30,16 +31,17 @@ const plumberNotify = (title) => {
 gulp.task("html", function () {
   return gulp
     .src("./src/*.html")
+    .pipe(changed("./dist/"))
     .pipe(plumber(plumberNotify("HTML")))
     .pipe(fileInclude(fileIncludeSetting))
     .pipe(gulp.dest("./dist/"));
 });
 
-
 gulp.task("sass", function () {
   return (
     gulp
       .src("./src/scss/**/*.scss")
+      .pipe(changed("./dist/css/"))
       .pipe(plumber(plumberNotify("SCSS")))
       .pipe(sourceMaps.init())
       .pipe(sass().on("error", sass.logError))
@@ -50,26 +52,33 @@ gulp.task("sass", function () {
 });
 
 gulp.task("images", function () {
-  return gulp.src("./src/img/**/*")
-  .pipe(imagemin({verbose: true}))
-  .pipe(gulp.dest("./dist/img/"));
+  return gulp
+    .src("./src/img/**/*")
+    .pipe(changed("./dist/img/"))
+    .pipe(imagemin({ verbose: true }))
+    .pipe(gulp.dest("./dist/img/"));
 });
 
 // gulp.task("fonts", function () {
-//   return gulp.src("./src/fonts/**/*").pipe(gulp.dest("./dist/fonts/"));
+//   return gulp.src("./src/fonts/**/*")
+//   .pipe(changed("./dist/fonts/"))
+//   .pipe(gulp.dest("./dist/fonts/"));
 // });
 
 // gulp.task("files", function () {
-//   return gulp.src("./src/files/**/*").pipe(gulp.dest("./dist/files/"));
+//   return gulp.src("./src/files/**/*")
+//   .pipe(changed("./dist/files/"))
+//   .pipe(gulp.dest("./dist/files/"));
 // });
 
 gulp.task("js", function () {
-  return gulp.src("./src/js/*.js")
-  .pipe(plumber(plumberNotify("JS")))
-  .pipe(babel())
-  .pipe(webpack(require("./webpack.config")))
-  .pipe(gulp.dest("./dist/js"));
-})
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(plumber(plumberNotify("JS")))
+    .pipe(babel())
+    .pipe(webpack(require("./webpack.config")))
+    .pipe(gulp.dest("./dist/js"));
+});
 
 const serverOptions = {
   livereload: true,
